8b433f95a6a2c63c1d1df1f4e7923d1c181c3236
adhemukhlis <adhemukhlis@gmail.com>
2022-10-19 17:27:01 +0700

    init PWA

--- src/index.js
+++ src/index.js
@@ -1,18 +1,40 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import reportWebVitals from './reportWebVitals'
+// import reportWebVitals from './reportWebVitals'
 
 import EntryPoint from '@/src/EntryPoint'
 
+import * as serviceWorker from './serviceWorker';
+
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
-	<React.StrictMode>
 		<EntryPoint />
-	</React.StrictMode>
 )
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals()
+serviceWorker.register({
+	onUpdate: registration => {
+		alert('New version available!  Ready to update?');
+		window
+			.location
+			.reload();
+		if (registration && registration.waiting) {
+			registration
+				.waiting
+				.postMessage({type: 'SKIP_WAITING'});
+			registration
+				.waiting
+				.addEventListener('statechange', e => {
+					if (e.target.state === 'activated') {
+						window
+							.location
+							.reload()
+					}
+				})
+		}
+	}
+});
+
